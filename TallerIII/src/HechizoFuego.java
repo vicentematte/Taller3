34d95b510d9b323e17106f24c3767e07d719761f
Vicente <Vicente@DESKTOP-LSSH2GD>
2026-06-07 14:20:52 -0400

    Implemento de la clase HechizoTierra

--- TallerIII/src/HechizoFuego.java
+++ TallerIII/src/HechizoFuego.java
@@ -1,4 +1,28 @@
 
-public class HechizoFuego {
+public class HechizoFuego extends Hechizo{
+	private int duracionQuemadura;
 
+	public HechizoFuego(String nombre, String tipo, int damage, int duracionQuemadura) {
+		super(nombre, tipo, damage);
+		this.duracionQuemadura = duracionQuemadura;
+	}
+
+	public int getDuracionQuemadura() {
+		return duracionQuemadura;
+	}
+
+	public void setDuracionQuemadura(int duracionQuemadura) {
+		this.duracionQuemadura = duracionQuemadura;
+	}
+
+	@Override
+	public String toString() {
+		return "HechizoFuego [duracionQuemadura=" + duracionQuemadura + "]";
+	}
+	
+	public double calcularPuntaje() {
+		return getDamage() * duracionQuemadura;
+	}
+	
+	
 }
