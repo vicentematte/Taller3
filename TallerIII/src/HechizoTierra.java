af43e0b15ed2dca4423aa5d8acdd2e75097ee539
Vicente <Vicente@DESKTOP-LSSH2GD>
2026-06-07 14:21:07 -0400

    Agregue la clase Hechizotierra

--- TallerIII/src/HechizoTierra.java
+++ TallerIII/src/HechizoTierra.java
@@ -1,4 +1,27 @@
 
-public class HechizoTierra {
+public class HechizoTierra extends Hechizo{
+	private int mejoraDefensa;
 
+	public HechizoTierra(String nombre, String tipo, int damage, int mejoraDefensa) {
+		super(nombre, tipo, damage);
+		this.mejoraDefensa = mejoraDefensa;
+	}
+
+	public int getMejoraDefensa() {
+		return mejoraDefensa;
+	}
+
+	public void setMejoraDefensa(int mejoraDefensa) {
+		this.mejoraDefensa = mejoraDefensa;
+	}
+
+	@Override
+	public String toString() {
+		return "HechizoTierra [mejoraDefensa=" + mejoraDefensa + "]";
+	}
+	
+	public double calcularPuntaje() {
+		return (getDamage() * mejoraDefensa) / 2;
+	}
+	
 }
